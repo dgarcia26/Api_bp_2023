a14eb853bf1431feceff13cf34006f1fd57f32cb
Darwin Garcia <dgarcia26@cuc.edu.co>
2023-06-02 22:45:06 -0500

    Api_YuGiHo

--- src/components/PeticionApi_yugiho.jsx
+++ src/components/PeticionApi_yugiho.jsx
@@ -1,6 +1,21 @@
 import React from 'react'
+import { useState } from 'react'
 
 const PeticionApi_yugiho = () => {
+    const [personajes, setPersonajes] = useState([])
+    const [paginacion, setPaginacion] = useState(1)
+
+    const traerPersonajes = async() =>{
+        try{
+            const res = await fetch(`https://db.ygoprodeck.com/api/v7/cardinfo.php?num=5&offset=${(paginacion - 1) * 5}`)
+            const respuesta = await res.json()
+            const auxPersonajes = respuesta.data
+            setPersonajes(auxPersonajes)
+        }catch(error){
+            console.log(error)
+        }
+    }
+
   return (
     <div>PeticionApi_yugiho</div>
   )
