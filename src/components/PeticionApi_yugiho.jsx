6db1fd4e87fb7e9534acd8ad328951b9f0fbd639
Darwin Garcia <dgarcia26@cuc.edu.co>
2023-06-02 22:39:05 -0500

    preparacion de los archivos

---
+++ src/components/PeticionApi_yugiho.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const PeticionApi_yugiho = () => {
+  return (
+    <div>PeticionApi_yugiho</div>
+  )
+}
+
+export default PeticionApi_yugiho
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import PeticionApi_yugiho from './components/PeticionApi_yugiho.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <PeticionApi_yugiho />
   </React.StrictMode>,
 )
