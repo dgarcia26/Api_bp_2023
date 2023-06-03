3858c81c897fad4e6d86238ea13b825e692b03fa
Darwin Garcia <dgarcia26@cuc.edu.co>
2023-06-02 22:42:16 -0500

    Footer

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,11 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import PeticionApi_yugiho from './components/PeticionApi_yugiho.jsx'
+import Footer from './components/Footer.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <PeticionApi_yugiho />
+    <Footer />
   </React.StrictMode>,
 )
